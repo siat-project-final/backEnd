ecb5b3c20f70f6edfd2cf27bf9d305c7c269e235
kth <kth1017@github.com>
2025-06-09 10:41:10 +0900

    first commit

---
+++ src/main/java/com/takoyakki/backend/BackendApplication.java
@@ -0,0 +1,13 @@
+package com.takoyakki.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+
+}
