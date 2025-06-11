69ffdb888ed92e3e182989333061be6f11a86d11
Suhyun Lee <122947409+suhyunle@users.noreply.github.com>
2025-06-11 22:51:56 +0900

    Update MentoringReservationMapper.java

--- src/main/java/com/takoyakki/backend/domain/mentoring/repository/MentoringReservationMapper.java
+++ src/main/java/com/takoyakki/backend/domain/mentoring/repository/MentoringReservationMapper.java
@@ -1,10 +1,15 @@
 package com.takoyakki.backend.domain.mentoring.repository;
 
+import com.takoyakki.backend.domain.mentoring.model.MentoringReservation;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 @Mapper
 public interface MentoringReservationMapper {
     List<MentoringReservation> findByDate(@Param("date") String date);
     void insertReservation(MentoringReservation reservation);
     void deleteReservation(@Param("id") Long id);
+   
 }
