5f9cdee756c55789719ae5e9772ee9680fae72a1
Suhyun Lee <122947409+suhyunle@users.noreply.github.com>
2025-06-11 22:47:40 +0900

    import 수정

--- src/main/java/com/takoyakki/backend/domain/mentoring/model/Mentoring.java
+++ src/main/java/com/takoyakki/backend/domain/mentoring/model/Mentoring.java
@@ -5,8 +5,8 @@ public class Mentoring {
     private Long id;
     private Long mentorId;
     private Long menteeId;
-    private String status; // 예약 대기, 예약 확정, 예약 취소
+    private String status; // 예약대기, 예약확정, 예약취소
     private String date;
     private String time;
-    private String cancelReason; // 예약 취소 사유
+    private String cancelReason; // 예약취소사유
 }
--- src/main/java/com/takoyakki/backend/domain/mentoring/service/MentoringService.java
+++ src/main/java/com/takoyakki/backend/domain/mentoring/service/MentoringService.java
@@ -1,5 +1,8 @@
 package com.takoyakki.backend.domain.mentoring.service;
 
+import com.takoyakki.backend.domain.mentoring.model.MentoringReservation;
+import java.util.List;
+
 public interface MentoringService {
     List<MentoringReservation> getReservationsByDate(String date);
     void createReservation(MentoringReservation reservation);
