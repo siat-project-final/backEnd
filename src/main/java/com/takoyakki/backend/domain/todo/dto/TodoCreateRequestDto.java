a7e9cdaceec51071f3713a1c1a28fb03609c9115
Suhyun Lee <122947409+suhyunle@users.noreply.github.com>
2025-06-19 10:05:25 +0900

    [refactor] 매퍼 추가 완료

--- src/main/java/com/takoyakki/backend/domain/todo/dto/TodoCreateRequestDto.java
+++ src/main/java/com/takoyakki/backend/domain/todo/dto/TodoCreateRequestDto.java
@@ -12,32 +12,25 @@ import lombok.*;
 @Schema(description = "할 일 생성 요청 DTO")
 public class TodoCreateRequestDto {
 
+    @Schema(description = "할 일 ID", example = "1")
+    private Long id;
+
     @NotBlank(message = "사용자 ID는 필수입니다.")
-    @Schema(description = "사용자 ID", example = "member123", required = true)
+    @Schema(description = "사용자 ID", example = "123")
     private String memberId;
 
-    @NotBlank(message = "내용은 비어 있을 수 없습니다.")
-    @Schema(description = "할 일 내용", example = "스터디 자료 정리", required = true)
+    @NotBlank(message = "내용은 필수입니다.")
+    @Schema(description = "할 일 내용", example = "스터디 준비")
     private String content;
 
-    @Pattern(regexp = "^\\d{4}-\\d{2}-\\d{2}$", message = "날짜 형식은 YYYY-MM-DD이어야 합니다.")
-    @Schema(description = "할 일 날짜 (yyyy-MM-dd)", example = "2025-06-18", required = true)
+    @Pattern(regexp = "^\\d{4}-\\d{2}-\\d{2}$", message = "날짜 형식은 yyyy-MM-dd 입니다.")
+    @Schema(description = "날짜", example = "2025-06-19")
     private String date;
 
-    @Pattern(regexp = "^\\d{2}:\\d{2}$", message = "시간 형식은 HH:mm이어야 합니다.")
-    @Schema(description = "할 일 시간 (HH:mm)", example = "14:30")
-    private String time;
-
-    @Schema(description = "할 일 중요 여부", example = "true")
+    @Schema(description = "중요 여부", example = "true")
     private boolean isImportant;
 
-    private Long id; // 생성 후 매퍼에서 세팅될 ID
-
     public Long getId() {
-        return this.id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
+        return id;
     }
 }
--- src/main/java/com/takoyakki/backend/domain/todo/dto/TodoUpdateRequestDto.java
+++ src/main/java/com/takoyakki/backend/domain/todo/dto/TodoUpdateRequestDto.java
@@ -12,18 +12,14 @@ import lombok.*;
 @Schema(description = "할 일 수정 요청 DTO")
 public class TodoUpdateRequestDto {
 
-    @NotBlank(message = "내용은 비어 있을 수 없습니다.")
-    @Schema(description = "할 일 내용", example = "회의 자료 준비")
+    @NotBlank(message = "내용은 필수입니다.")
+    @Schema(description = "할 일 내용")
     private String content;
 
-    @Pattern(regexp = "^\\d{4}-\\d{2}-\\d{2}$", message = "날짜 형식은 YYYY-MM-DD이어야 합니다.")
-    @Schema(description = "할 일 날짜 (yyyy-MM-dd)", example = "2025-06-18")
+    @Pattern(regexp = "^\\d{4}-\\d{2}-\\d{2}$", message = "날짜 형식은 yyyy-MM-dd 입니다.")
+    @Schema(description = "날짜")
     private String date;
 
-    @Pattern(regexp = "^\\d{2}:\\d{2}$", message = "시간 형식은 HH:mm이어야 합니다.")
-    @Schema(description = "할 일 시간 (HH:mm)", example = "15:30")
-    private String time;
-
-    @Schema(description = "할 일 중요 여부", example = "false")
+    @Schema(description = "중요 여부")
     private boolean isImportant;
 }
