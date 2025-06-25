7b51bbb6d97e6523d29bc1848a04e9e46f57d5dc
kth <kth1017@github.com>
2025-06-25 10:13:25 +0900

    [feat] 통신 : 알림 조회 화면 추가 중

---
+++ src/main/java/com/takoyakki/backend/domain/notification/service/NotificationService.java
@@ -0,0 +1,12 @@
+package com.takoyakki.backend.domain.notification.service;
+
+import com.takoyakki.backend.domain.notification.dto.response.NotificationToMenteeSelectResponseDto;
+import com.takoyakki.backend.domain.notification.dto.response.NotificationToMentorSelectResponseDto;
+
+import java.util.List;
+
+public interface NotificationService {
+    List<NotificationToMentorSelectResponseDto> selectNotificationToMentor(Long memberId);
+
+    List<NotificationToMenteeSelectResponseDto> selectNotificationToMentee(Long memberId);
+}
