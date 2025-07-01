8b390d51c69c0264ecd910312c036df33068f7d5
kth <kth1017@github.com>
2025-07-01 16:13:02 +0900

    배포 test : rds 연결

--- src/main/java/com/takoyakki/backend/common/config/WebMvcConfig.java
+++ src/main/java/com/takoyakki/backend/common/config/WebMvcConfig.java
@@ -26,7 +26,8 @@ public class WebMvcConfig implements WebMvcConfigurer {
         corsRegistry
                 .addMapping("/**")
                 .allowedHeaders("*")
-                .allowedOrigins("http://localhost:3000")
+//                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://siat-takoyakki-v1.s3-website.ap-northeast-2.amazonaws.com")
                 .allowedMethods("*")
                 .allowCredentials(true);
     }
